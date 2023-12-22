063eab1a9260a3dd2ca7275b7681fb0c4a1716ef
MakoskiVictor <makoski.ed@gmail.com>
2023-12-21 22:48:30 -0300

    add front almost finished

---
+++ Client/src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import { Card } from "../components/Card";
+import { CHARACTERS_URL } from "../constants";
+import { useGetCharacters } from "../server/useGetCharacters";
+import style from "../css/home.module.css";
+
+export default function Home() {
+  const { data, loading /* handleCancelRequest, error */ } =
+    useGetCharacters(CHARACTERS_URL);
+
+  return (
+    <section className={style.section}>
+      {loading && <p>Loading...</p>}
+
+      {data &&
+        data.map((c) => (
+          <Card
+            className={style.card}
+            name={c.name}
+            gender={c.gender}
+            house={c.house}
+            image={c.image}
+            key={c.id}
+          />
+        ))}
+    </section>
+  );
+}
